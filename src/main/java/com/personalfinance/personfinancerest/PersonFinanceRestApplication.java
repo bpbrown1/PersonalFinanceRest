6bc5a750634f1c73beaf04f67a7ebc68b3badf47
Bradley Brown <bradleybrown04@gmail.com>
2026-08-22 13:31:35 -0500

    Initial commit

---
+++ src/main/java/com/personalfinance/personfinancerest/PersonFinanceRestApplication.java
@@ -0,0 +1,13 @@
+package com.personalfinance.personfinancerest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonFinanceRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersonFinanceRestApplication.class, args);
+    }
+
+}
